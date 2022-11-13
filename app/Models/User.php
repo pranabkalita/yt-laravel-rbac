394f836587ff1d73f2c4626123bc85cb789cfe0b
Pranab Kalita <pavankalita@gmail.com>
2022-11-13 15:18:42 +0530

    Created common middleware.

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,4 +46,9 @@ class User extends Authenticatable
     {
         return $this->belongsTo(Role::class);
     }
+
+    public function hasRole(string $name)
+    {
+        return $this->role()->where('name', $name)->exists();
+    }
 }
