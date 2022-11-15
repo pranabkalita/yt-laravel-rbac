db52098f029d92b0e975d63bf039ee68c637d4a1
Pranab Kalita <pavankalita@gmail.com>
2022-11-15 14:04:44 +0530

    Created the Users List

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -25,6 +25,11 @@
                         :active="request()->routeIs('admin.permissions.index')">
                         Permissions
                     </x-nav-link>
+
+                    <x-nav-link :href="route('admin.users.index')"
+                        :active="request()->routeIs('admin.users.index')">
+                        Users
+                    </x-nav-link>
                     @endif
                 </div>
             </div>
